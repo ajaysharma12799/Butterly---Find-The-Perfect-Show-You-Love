58e338ca058198838a6400ceea5d296448fa1929
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-01-11 14:30:36 +0530

    Fixed Broken Changes

--- src/components/MediaContainer/MediaContainer.js
+++ src/components/MediaContainer/MediaContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Box } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
